79c2e2bdbcb8afc765455f467c05bd62d6528db3
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-01-12 11:55:52 +0500

    Manage Invoices

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <script src="{{ asset('js/jquery.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{asset('plugins/sweetalert2/sweetalert2.min.js')}}"></script>
-    <script src="https://unpkg.com/imask"></script>
+    <script src="{{ asset('js/imask.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
